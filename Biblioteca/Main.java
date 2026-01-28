564339ca14aa9006dcd6dfd3735f5102c004374a
Luisa Chavarría <141459402+Luma2006@users.noreply.github.com>
2026-01-27 20:17:57 -0500

    Agregar inicialización de Scanner y objeto Biblioteca en la clase Main

--- Biblioteca/Main.java
+++ Biblioteca/Main.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Biblioteca biblioteca = new Biblioteca();
+        
+
+
     }
 }
